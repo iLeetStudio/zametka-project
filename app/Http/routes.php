0fc4b57f5fbbb3d62bee3563d7385103fcc6fc78
Archakov <abreak06@gmail.com>
2016-06-19 23:20:08 +0300

    Подготовка роутингов

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,10 +11,45 @@
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::group(['middleware' => 'web'], function() {
 
-Route::get('/{url}', function () {
-    return view('note');
+	/* Роутинг на главную сайта */
+
+	Route::get('/', [
+		'as' => 'index',
+		'uses' => 'HomeController@index'
+	]);
+
+	/* NoteController - Роутинг контроллера заметок */
+
+	Route::get('/{url}', [
+		'as' => 'index',
+		'uses' => 'NoteController@getNote'
+	]);
+
+	Route::get('/add', [
+		'as' => 'index',
+		'uses' => 'NoteController@addNote'
+	]);
+
+	Route::get('/{url}/delete', [
+		'as' => 'index',
+		'uses' => 'NoteController@deleteNote'
+	]);
+
+	Route::get('/{url}/edit', [
+		'as' => 'index',
+		'uses' => 'NoteController@editNote'
+	]);
+
+	Route::post('/{url}/save', [
+		'as' => 'index',
+		'uses' => 'NoteController@saveNote'
+	]);
+
+	Route::get('/finish', [
+		'as' => 'index',
+		'uses' => 'NoteController@finish'
+	]);
+
 });
